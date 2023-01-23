c0df47cd86e33dbe6781b4c871c5a277092a3e35
devrabaioli <devrabaioli@gmail.com>
2023-01-23 16:41:11 +0000

    Firts Commit

---
+++ src/main/java/devrabaioli/McstsApplication.java
@@ -0,0 +1,13 @@
+package devrabaioli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McstsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McstsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/devrabaioli/McstsApplicationTests.java
@@ -0,0 +1,13 @@
+package devrabaioli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McstsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
